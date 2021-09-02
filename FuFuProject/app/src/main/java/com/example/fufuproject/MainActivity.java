1019ff4383773a81e029dac3b103a65d8f7d41cf
JiEun2001 <farhanazmi012@gmail.com>
2021-09-02 15:22:38 +0800

    prob

--- FuFuProject/app/src/main/java/com/example/fufuproject/MainActivity.java
+++ FuFuProject/app/src/main/java/com/example/fufuproject/MainActivity.java
@@ -45,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         userRegistration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
             }
         });
