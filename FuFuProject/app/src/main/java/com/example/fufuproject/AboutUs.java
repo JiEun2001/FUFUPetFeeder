15d697eb72eea97f4c28bf887be12b84c94d5da8
jasonhakimi <jasononlineclass@gmail.com>
2021-10-23 17:17:41 +0800

    About Us and Contact Us function

--- FuFuProject/app/src/main/java/com/example/fufuproject/AboutUs.java
+++ FuFuProject/app/src/main/java/com/example/fufuproject/AboutUs.java
@@ -3,6 +3,7 @@ package com.example.fufuproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -10,6 +11,10 @@ import android.widget.ImageView;
 public class AboutUs extends AppCompatActivity {
 
     ImageView IvJason;
+    ImageView IvFarhan;
+    ImageView IvFaiz;
+    ImageView IvDina;
+    ImageView IvHasya;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +27,41 @@ public class AboutUs extends AppCompatActivity {
                 startActivity(new Intent(AboutUs.this, AdminJason.class));
             }
         });
+        setContentView(R.layout.activity_about_us);
+        IvFarhan = (ImageView) findViewById(R.id.ivfarhan);
+        IvFarhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AboutUs.this, AdminFarhan.class));
+            }
+        });
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+        IvFaiz = (ImageView) findViewById(R.id.ivfaiz);
+        IvFaiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AboutUs.this, AdminFaiz.class));
+            }
+        });
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+        IvDina = (ImageView) findViewById(R.id.ivdina);
+        IvDina.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AboutUs.this, AdminDina.class));
+            }
+        });
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+        IvHasya = (ImageView) findViewById(R.id.ivhasya);
+        IvHasya.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AboutUs.this, AdminHasya.class));
+            }
+        });
     }
+
 }
---
+++ FuFuProject/app/src/main/java/com/example/fufuproject/AdminFarhan.java
@@ -0,0 +1,14 @@
+package com.example.fufuproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminFarhan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_farhan);
+    }
+}
