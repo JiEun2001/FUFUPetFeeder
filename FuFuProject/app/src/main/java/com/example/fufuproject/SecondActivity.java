ea2b99082b57d0b656ce0c11b6eb826b81486a61
JiEun2001 <farhanazmi012@gmail.com>
2021-10-30 19:57:52 +0800

    View history, button manual and read distance for member

--- FuFuProject/app/src/main/java/com/example/fufuproject/SecondActivity.java
+++ FuFuProject/app/src/main/java/com/example/fufuproject/SecondActivity.java
@@ -9,13 +9,26 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SecondActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
-    private Button logout;
+    private Button logout,historyuser;
+    private TextView Distance;
+    private ToggleButton Manualbtnuser;
+
+    String valueDistanceUser;
+    String valueMotorUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +40,55 @@ public class SecondActivity extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
 
         logout = (Button)findViewById(R.id.btnLogout);
+        Distance = (TextView)findViewById(R.id.DistanceTVuser);
+        Manualbtnuser = (ToggleButton)findViewById(R.id.manualBtnuser);
+        historyuser = (Button) findViewById(R.id.btnHistoryuser);
+
+
+
+        DatabaseReference databaseReference;
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                valueDistanceUser = snapshot.child("ESP32/distance").getValue().toString();
+                Distance.setText(valueDistanceUser);
+
+                valueMotorUser = snapshot.child("ESP32/motor").getValue().toString();
+                if(valueMotorUser.equals("0"))
+                    Manualbtnuser.setChecked(false);
+                else
+                    Manualbtnuser.setChecked(true);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        Manualbtnuser.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    DatabaseReference motorRef = FirebaseDatabase.getInstance().getReference("ESP32/motor");
+                    motorRef.setValue(1);
+
+                }else{
+                    DatabaseReference motorRef = FirebaseDatabase.getInstance().getReference("ESP32/motor");
+                    motorRef.setValue(0);
+                }
+
+            }
+        });
+
+        historyuser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SecondActivity.this, ViewHistoryUser.class));
+            }
+        });
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
